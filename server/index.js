82c9cebdc5e5293a56ad2943444810e05970d7b4
938938 <938kjh@gmail.com>
2023-01-11 23:13:22 +0900

    ::node 서버 구성 중

---
+++ server/index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = process.env.port || 4000;
+
+app.use(express.json());
+const cors = require('cors');
+app.use(cors());
+
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+app.get('/category', (req, res) => {
+  const category = ['전체 카테고리', 'JAVA', 'Javascript', 'Database'];
+  res.send(category);
+});
+
+// app.listen(port, () => {
+//   console.log(`Example app listening at http://localhost:${port}`);
+// });
+
+const mysql = require('mysql');
+
+// sql 연동
+const db = mysql.createConnection({
+  user: 'root',
+  host: 'localhost',
+  password: '0909',
+  database: 'bbs',
+});
+
+db.connect();
+
+// 잘 연동 되었는지 확인
+db.connect(function (err) {
+  if (err) throw err;
+  console.log('DB is Connected!');
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
