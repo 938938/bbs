ee84b6bb6a0279e588adb0ef8d11ea72944a9c6d
938938 <938kjh@gmail.com>
2023-01-28 23:57:33 +0900

    ::API 분리

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -2,11 +2,33 @@ import axios from 'axios';
 
 const host = 'http://localhost:4000/api';
 
+// api 버전 명시해주는 것이 좋음
+
 export const getCategory = async () => {
   await axios
     .get(`${host}/category`)
     .then((res) => {
       return res.data;
+    })
+    .catch((err) => console.log(err));
+};
+
+export const getBoardData = async () => {
+  await axios
+    .get(`${host}/list`)
+    .then((res) => {
+      console.log(res.data);
+      setData(res.data);
+    })
+    .catch((err) => console.log(err));
+};
+
+export const getPostData = async () => {
+  await axios
+    .get(`${host}/post/${id}`)
+    .then((res) => {
+      console.log(res.data);
+      setData(res.data[0]);
     })
     .catch((err) => console.log(err));
 };
---
+++ client/src/components/Write.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Write = () => {
+  return (
+    <div>Write</div>
+    // 폼 작성할 곳
+  )
+}
+
+export default Write
