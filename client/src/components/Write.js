2923ad5e5802df1fb0dc6e2482f200442cf47bff
938938 <938kjh@gmail.com>
2023-01-31 23:54:15 +0900

    ::작성, 수정 분리 진행 중

--- client/src/components/Write.js
+++ client/src/components/Write.js
@@ -1,10 +1,37 @@
-import React from 'react'
+import React from 'react';
 
 const Write = () => {
   return (
-    <div>Write</div>
-    // 폼 작성할 곳
-  )
-}
+    <>
+      <div>
+        <span>작성자</span>
+        <input type='text' autoComplete='off' />
+      </div>
+      <div>
+        <span>비밀번호</span>
+        <div>
+          <input type='password' autoComplete='off' />
+          <input type='password' />
+        </div>
+      </div>
+      <div>
+        <span>제목</span>
+        <input type='text' />
+      </div>
+      <div>
+        <span>내용</span>
+        <textarea />
+      </div>
+      <button
+        onClick={() => {
+          navigate(-1);
+        }}
+      >
+        취소
+      </button>
+      <button>저장</button>
+    </>
+  );
+};
 
-export default Write
+export default Write;
